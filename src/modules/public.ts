762fbf142d7d44f77ad51a5d1b3f6dc0e5d1fde3
Nikita Fuchs <q.fuchs@gmail.com>
2023-10-17 10:46:35 +0200

    building demo fails silently after exporting objects and array

--- src/modules/public.ts
+++ src/modules/public.ts
@@ -13,3 +13,21 @@ export * from "./select/public";
 export * from "./sidebar/public";
 export * from "./tabs/public";
 export * from "./transition/public";
+
+import { SuiAccordionModule } from "./accordion/public";
+import { SuiCheckboxModule } from "./checkbox/public";
+import { SuiCollapseModule } from "./collapse/public";
+import { SuiDatepickerModule } from "./datepicker/public";
+import { SuiDimmerModule } from "./dimmer/public";
+import { SuiDropdownModule } from "./dropdown/public";
+import { SuiModal } from "./modal/public";
+import { SuiPopupModule } from "./popup/public";
+import { SuiProgressModule } from "./progress/public";
+import { SuiRatingModule } from "./rating/public";
+import { SuiSearchModule } from "./search/public";
+import { SuiSelectModule } from "./select/public";
+import { SuiSidebarModule } from "./sidebar/public";
+import { SuiTabsModule } from "./tabs/public";
+import { SuiTransitionModule } from "./transition/public";
+
+export let SuiModule = [SuiAccordionModule, SuiCheckboxModule, SuiCollapseModule, SuiDatepickerModule, SuiDimmerModule, SuiDropdownModule, SuiModal, SuiPopupModule, SuiProgressModule, SuiRatingModule, SuiSearchModule, SuiSelectModule, SuiSidebarModule, SuiTabsModule, SuiTransitionModule]
