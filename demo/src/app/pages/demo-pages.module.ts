e4288b1519bebf91fb7027b0ee5d1ebc0b4120cb
Nikita Fuchs <q.fuchs@gmail.com>
2023-10-17 13:02:27 +0200

    milestone: add ngModule to public API for importing all modules at once

--- demo/src/app/pages/demo-pages.module.ts
+++ demo/src/app/pages/demo-pages.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import * as SuiModule from "ng2-semantic-ui";
+// import * as SuiModule from "ng2-semantic-ui";
+// import SuiModule from "ng2-semantic-ui";
+import { SuiModule } from "ng2-semantic-ui";
 import { DemoComponentsModule } from "../components/demo-components.module";
 
 import { GettingStartedPage } from "./getting-started/getting-started.page";
@@ -40,24 +42,7 @@ import { TestPage } from "./development/test/test.page";
         FormsModule,
         ReactiveFormsModule,
         RouterModule,
-        SuiModule.SuiAccordionModule,
-        SuiModule.SuiSelectModule,
-        SuiModule.SuiSidebarModule,
-        SuiModule.SuiMessageModule,
-        SuiModule.SuiPaginationModule,
-        SuiModule.SuiCheckboxModule,
-        SuiModule.SuiCollapseModule,
-        SuiModule.SuiDatepickerModule,
-        SuiModule.SuiDimmerModule,
-        SuiModule.SuiDropdownModule,
-        SuiModule.SuiModalModule,
-        SuiModule.SuiPopupModule,
-        SuiModule.SuiProgressModule,
-        SuiModule.SuiRatingModule,
-        SuiModule.SuiSearchModule,
-        SuiModule.SuiAccordionModule,
-        SuiModule.SuiTransitionModule,
-        SuiModule.SuiTabsModule,
+        SuiModule,
         DemoComponentsModule
     ],
     declarations: [
